fe252b5ef253f3e353af0ff9d6a57bf189989b2b
Rian <142219160+Rianofc@users.noreply.github.com>
2024-06-21 09:01:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,29 @@ const PORT = process.env.PORT || 3000;
 const YOUTUBE_API_KEY = 'AIzaSyB1bRFJEil3Mf_KUFhQiWXUWedAERxXbt4'; // Ganti dengan API Key Anda
 
 app.use(express.static('public'));
+app.get('/download', async (req, res) => {
+    const url = req.query.url;
+    const resolution = req.query.resolution;
 
+    if (!url) {
+        return res.status(400).send('URL video YouTube diperlukan.');
+    }
+
+    try {
+        const info = await ytdl.getInfo(url);
+        const format = ytdl.chooseFormat(info.formats, { quality: resolution });
+
+        if (!format) {
+            return res.status(400).send('Resolusi tidak ditemukan.');
+        }
+
+        res.header('Content-Disposition', `attachment; filename="${info.videoDetails.title}.mp4"`);
+        ytdl(url, { format }).pipe(res);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Terjadi kesalahan saat memproses permintaan Anda.');
+    }
+});
 app.get('/search', async (req, res) => {
     const query = req.query.q;
     const response = await axios.get(`https://www.googleapis.com/youtube/v3/search`, {
@@ -23,6 +45,8 @@ app.get('/search', async (req, res) => {
 
 app.get('/audio', (req, res) => {
     const url = req.query.url;
+    const info = await ytdl.getInfo(url);
+            res.header('Content-Disposition', `attachment; filename="${info.videoDetails.title}.mp3"`);
     ytdl(url, { filter: 'audioonly' }).pipe(res);
 });
 
