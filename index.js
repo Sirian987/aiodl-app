8bc08160dcf877a4ba7e046c7127ab45550fe052
Rian <142219160+Rianofc@users.noreply.github.com>
2024-12-01 12:09:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const ytdl = require('@distube/ytdl-core');
 const fetch = require('node-fetch');
+const axios = require('axios');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
